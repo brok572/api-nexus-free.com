565a9fd77d959c95dca8bf4e04d27a80b5393e87
brok572 <kofwar1010@gmail.com>
2026-05-25 15:24:57 +0300

    Update time.js

--- api/util/time.js
+++ api/util/time.js
@@ -1,21 +1,28 @@
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
   
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
   try {
-    const r = await fetch('https://worldtimeapi.org/api/ip');
+    const r = await fetch('https://timeapi.io/api/Time/current/ip');
     const data = await r.json();
 
     res.json({
       status: true,
-      timezone: data.timezone,
-      datetime: data.datetime,
-      day_of_week: data.day_of_week,
+      timezone: data.timeZone,
+      datetime: data.dateTime,
+      date: data.date,
+      time: data.time,
       author: "@nexus - Nexus API"
     });
   } catch (err) {
     res.status(500).json({
       status: false,
       message: "Failed to get time",
+      error: err.message,
       author: "@nexus - Nexus API"
     });
   }
